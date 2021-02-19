7cc0b719d7059a34d9e580eb60d39731c11529fa
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-19 11:04:10 -0500

    changed port

--- server.js
+++ server.js
@@ -119,6 +119,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-const listener = app.listen(process.env.PORT || 3001, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
