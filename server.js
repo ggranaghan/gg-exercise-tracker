0619edb8fa3269b510fe3df359d444d3ded03b43
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-19 12:11:47 -0500

    removed require dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 const path = require('path');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
