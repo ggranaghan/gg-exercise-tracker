2cb9252886feedde589896f7172b250a6bdae728
ggranaghan <56278351+ggranaghan@users.noreply.github.com>
2021-02-19 12:20:48 -0500

    debugging

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
+console.log(process.env.MONGO_URI)
 //initiate database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const UserSchema = new mongoose.Schema({
